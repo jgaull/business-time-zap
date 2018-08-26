60c780c5186233d4a58eb2ade5a7d5b66eb88ad9
Jonathan Gaull <jonbeesh@gmail.com>
2018-08-25 19:40:21 -0700

    Moved url to environment variables

--- utils.js
+++ utils.js
@@ -27,8 +27,8 @@ function getUrl(bundle, path) {
     }
     
     workingHours = useDefaultHours ? undefined : workingHours;
-
-    var url = UrlAssembler('https://moment-business-days.herokuapp.com' + path)
+    
+    var url = UrlAssembler(process.env.API_URL + path)
         .query({
             date: bundle.inputData.date,
             format: bundle.inputData.format,

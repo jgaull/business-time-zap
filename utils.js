026a61aa42daf80ba91ce548eee9be8ecf13c4e7
Jon Gaull <jon@bikepretty.com>
2018-08-27 22:23:43 -0700

    Business hours is now a much better user experience

--- utils.js
+++ utils.js
@@ -5,35 +5,31 @@ const moment = require('moment');
 function getUrl(bundle, path, z) {
 
     var workingHours = {};
-
     var days = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
     var outputBusinessHoursFormat = 'HH:mm:ss';
     var inputBusinessHoursFormat = bundle.inputData.businessHoursFormat;
-    z.console.log('inputData: ' + JSON.stringify(bundle.inputData));
     var useDefaultHours = true;
     for (var i = 0; i < days.length; i++) {
 
         var day = days[i];
+        var hours = null;
         var hoursString = bundle.inputData[day];
-
         if (hoursString) {
 
             hoursString.replace(/\s/g, '');
             var hours = hoursString.split('-');
-
             var open = momentOrNull(hours[0], inputBusinessHoursFormat);
             var close = momentOrNull(hours[1], inputBusinessHoursFormat);
-
+            
             if (open && open.isValid() && close && close.isValid()) {
                 useDefaultHours = false;
-                workingHours[i] = [open.format(outputBusinessHoursFormat), close.format(outputBusinessHoursFormat)];
-            }
-            else {
-                workingHours[i] = null;
+                hours = [open.format(outputBusinessHoursFormat), close.format(outputBusinessHoursFormat)];
             }
         }
+
+        workingHours[i] = hours;
     }
-    z.console.log('workingHours: ' + JSON.stringify(workingHours));
+
     workingHours = useDefaultHours ? undefined : workingHours;
     
     var url = UrlAssembler(process.env.API_URL + path)
@@ -53,7 +49,7 @@ function getWorkingHoursFields() {
     var helpText = 'Written as `9:00am-5:00pm` or `9:00-17:00` depending on the format chosen above. Leave blank if the business is closed. Leave all days blank to use default business hours (9am-5pm M-F).';
     return {
         key: 'businesshours', label: 'Business Hours', children: [
-            { key: 'businessHoursFormat', label: 'Format', choices: {'h:mma':'1:00pm','H:mm':'13:00'}},
+            { key: 'businessHoursFormat', label: 'Format', choices: getTimeFormats()},
             { key: 'sunday', label: 'Sunday', type: 'datetime', required: false, helpText: helpText },
             { key: 'monday', label: 'Monday', type: 'datetime', required: false },
             { key: 'tuesday', label: 'Tuesday', type: 'datetime', required: false },
@@ -63,6 +59,14 @@ function getWorkingHoursFields() {
             { key: 'saturday', label: 'Saturday', type: 'datetime', required: false }
         ]
     }
+}
+
+function getTimeFormats() {
+    return {'h:mma':'1:00pm','H:mm':'13:00'}
+}
+
+function getDateFormats() {
+    return {};
 }
 
 function momentOrNull(string, format) {
@@ -76,3 +80,5 @@ function momentOrNull(string, format) {
 module.exports.getUrl = getUrl;
 module.exports.momentOrNull = momentOrNull;
 module.exports.getWorkingHoursFields = getWorkingHoursFields;
+module.exports.getTimeFormats = getTimeFormats;
+module.exports.getDateFormats = getDateFormats;
